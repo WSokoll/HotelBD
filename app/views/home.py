fe7ee61ab244ee9f0b390c271a524148e7c94eda
WSokoll <wladek.sokol01@gmail.com>
2023-02-01 15:04:56 +0100

    home.py update + requirements

--- app/views/home.py
+++ app/views/home.py
@@ -1,11 +1,18 @@
 from flask import Blueprint, render_template
+from flask_login import current_user
 
 bp = Blueprint('home', __name__, template_folder='templates')
 
 
-# Show home page
 # TODO login required
-@bp.route('/home', methods=['GET'])
-@bp.route('/', methods=['GET'])
-def get():
+@bp.route('/home', methods=['GET', 'POST'])
+@bp.route('/', methods=['GET', 'POST'])
+def get_post():
+
+    if current_user.guest_id is not None:
+        pass
+
+    elif current_user.employee_id is not None:
+        pass
+
     return render_template('home.jinja')
