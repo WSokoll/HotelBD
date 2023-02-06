93ba2c0b93e26434b976fa88726dfe411cae99e4
WSokoll <wladek.sokol01@gmail.com>
2023-02-06 15:16:42 +0100

    Admin panel update

--- app/admin.py
+++ app/admin.py
@@ -11,14 +11,17 @@ class EmployeesModelView(ModelView):
     can_edit = True
     can_delete = True
     column_list = [
-        'first_name', 'last_name', 'position_id'
+        'first_name', 'last_name', 'position.name'
     ]
     column_details_list = [
-        'id', 'first_name', 'last_name', 'position_id', 'tel_number', 'email'
+        'id', 'first_name', 'last_name', 'position_id', 'position.name', 'tel_number', 'email'
     ]
     form_columns = [
         'first_name', 'last_name', 'position_id', 'tel_number', 'email'
     ]
+    column_labels = {
+        'position.name': 'Position Name'
+    }
 
 
 class TasksModelView(ModelView):
@@ -27,14 +30,17 @@ class TasksModelView(ModelView):
     can_edit = True
     can_delete = True
     column_list = [
-        'room_id', 'name'
+        'room.number', 'name'
     ]
     column_details_list = [
-        'id', 'room_id', 'name', 'employee_id', 'description'
+        'id', 'room_id', 'room.number', 'name', 'employee_id', 'description'
     ]
     form_columns = [
         'room_id', 'name', 'employee_id', 'description'
     ]
+    column_labels = {
+        'room.number': 'Room Number'
+    }
 
 
 class RoomsModelView(ModelView):
@@ -59,14 +65,17 @@ class RoomReservationsModelView(ModelView):
     can_edit = True
     can_delete = True
     column_list = [
-        'room_id', 'start_date', 'end_date', 'num_of_people'
+        'room.number', 'start_date', 'end_date', 'num_of_people'
     ]
     column_details_list = [
-        'id', 'room_id', 'start_date', 'end_date', 'num_of_people', 'guest_id'
+        'id', 'room_id', 'room.number',  'start_date', 'end_date', 'num_of_people', 'guest_id'
     ]
     form_columns = [
         'room_id', 'start_date', 'end_date', 'num_of_people', 'guest_id'
     ]
+    column_labels = {
+        'room.number': 'Room Number'
+    }
 
 
 class EquipmentModelView(ModelView):
@@ -75,14 +84,17 @@ class EquipmentModelView(ModelView):
     can_edit = True
     can_delete = True
     column_list = [
-        'cat_id', 'name', 'cost_per_hour'
+        'category.name', 'name', 'cost_per_hour'
     ]
     column_details_list = [
-        'id', 'cat_id', 'name', 'description', 'cost_per_hour'
+        'id', 'category.name', 'cat_id', 'name', 'description', 'cost_per_hour'
     ]
     form_columns = [
         'cat_id', 'name', 'description', 'cost_per_hour'
     ]
+    column_labels = {
+        'category.name': 'Category'
+    }
 
 
 class EqReservationsModelView(ModelView):
@@ -91,13 +103,32 @@ class EqReservationsModelView(ModelView):
     can_edit = True
     can_delete = True
     column_list = [
-        'equipment_id', 'start_date', 'end_date'
+        'equipment.name', 'start_date', 'end_date'
     ]
     column_details_list = [
-        'id', 'equipment_id', 'start_date', 'end_date', 'guest_id'
+        'id', 'equipment_id', 'equipment.name', 'start_date', 'end_date', 'guest_id'
     ]
     form_columns = [
         'equipment_id', 'start_date', 'end_date', 'guest_id'
+    ]
+    column_labels = {
+        'equipment.name': 'Equipment'
+    }
+
+
+class GuestsModelView(ModelView):
+    can_view_details = True
+    can_create = False
+    can_edit = True
+    can_delete = True
+    column_list = [
+        'first_name', 'last_name', 'email'
+    ]
+    column_details_list = [
+        'id', 'first_name', 'last_name', 'email', 'age', 'tel_number'
+    ]
+    form_columns = [
+        'first_name', 'last_name', 'email', 'age', 'tel_number'
     ]
 
 
@@ -116,6 +147,7 @@ def admin_panel_init(admin, db):
 
     admin.add_view(EmployeesModelView(app.models.Employees, db.session, name='Employees'))
     admin.add_view(TasksModelView(app.models.Tasks, db.session, name='Tasks'))
+    admin.add_view(GuestsModelView(app.models.Guests, db.session, name='Guests'))
     admin.add_view(RoomsModelView(app.models.Rooms, db.session, name='Rooms'))
     admin.add_view(RoomReservationsModelView(app.models.RoomReservations, db.session, name='R.Reservations'))
     admin.add_view(EquipmentModelView(app.models.Equipment, db.session, name='Equipment'))
--- app/models.py
+++ app/models.py
@@ -16,14 +16,16 @@ class Guests(db.Model):
     __table_args__ = {'extend_existing': True}
 
 
+class Positions(db.Model):
+    __tablename__ = 'positions'
+    __table_args__ = {'extend_existing': True}
+
+
 class Employees(db.Model):
     __tablename__ = 'employees'
     __table_args__ = {'extend_existing': True}
 
-
-class Positions(db.Model):
-    __tablename__ = 'positions'
-    __table_args__ = {'extend_existing': True}
+    position = db.relationship('Positions', foreign_keys='Employees.position_id')
 
 
 class Rooms(db.Model):
@@ -35,10 +37,7 @@ class RoomReservations(db.Model):
     __tablename__ = 'room_reservations'
     __table_args__ = {'extend_existing': True}
 
-
-class Equipment(db.Model):
-    __tablename__ = 'equipment'
-    __table_args__ = {'extend_existing': True}
+    room = db.relationship('Rooms', foreign_keys='RoomReservations.room_id')
 
 
 class EqCategories(db.Model):
@@ -46,11 +45,22 @@ class EqCategories(db.Model):
     __table_args__ = {'extend_existing': True}
 
 
+class Equipment(db.Model):
+    __tablename__ = 'equipment'
+    __table_args__ = {'extend_existing': True}
+
+    category = db.relationship('EqCategories', foreign_keys='Equipment.cat_id')
+
+
 class EqReservations(db.Model):
     __tablename__ = 'eq_reservations'
     __table_args__ = {'extend_existing': True}
+
+    equipment = db.relationship('Equipment', foreign_keys='EqReservations.equipment_id')
 
 
 class Tasks(db.Model):
     __tablename__ = 'tasks'
     __table_args__ = {'extend_existing': True}
+
+    room = db.relationship('Rooms', foreign_keys='Tasks.room_id')
